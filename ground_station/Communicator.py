606d8f16a1b10b0694d6c76dedeb0b4aa064ad0c
JackBlair87 <60362058+JackBlair87@users.noreply.github.com>
2020-06-23 19:11:44 -0400

    Added Communicator Info

--- ground_station/Communicator.py
+++ ground_station/Communicator.py
@@ -1,11 +1,21 @@
+#/dev/tty.HC-05-DevB
+import serial
+import time
+
 class Communicator:
-  def __init__(self, l_stop=90, r_stop=90):
-    self.l_stop_power = l_stop
-    self.r_stop_power = r_stop
-  def get_data(self):
-    #return new data if any, otherwise return None
-    return None
-  def set_servos(self, l_power, r_power):
-    pass
-  def stop_all(self):
-    self.set_servos(self.l_stop_power, self.r_stop_power)
+  def __init__(self, port = "/dev/tty.HC-05-DevB", baud = 9600):
+    self.port = port
+    self.baud = baud
+    
+  def start_test(self):
+    print("Start")
+    bluetooth=serial.Serial(port, 9600)#Start communications with the bluetooth unit
+    print("Connected")
+    bluetooth.flushInput() #This gives the bluetooth a little kick
+    for i in range(500): #send 5 groups of data to the bluetooth
+	    print("Ping")
+	    bluetooth.write(b"BOOP "+str.encode(str(i)))#These need to be bytes not unicode, plus a number
+	    input_data=bluetooth.readline() #This reads the incoming data
+	    print(input_data.decode())#These are bytes coming in so a decode is needed
+	    time.sleep(0.1) #A pause between bursts
+    bluetooth.close() #Otherwise the connection will remain open until a timeout which ties up the /dev/thingamabob
