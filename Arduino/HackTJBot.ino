47beb5ff5fe826ad50a3876ca5fc51284e782465
JackBlair87 <60362058+JackBlair87@users.noreply.github.com>
2020-07-06 19:14:43 -0400

    Added RGB Functionality and Polished Bluetooth

--- Arduino/HackTJBot.ino
+++ Arduino/HackTJBot.ino
@@ -1,3 +1,4 @@
+#include <RGBLamp.h> //RGB LED Library
 #include <TimedAction.h> //MultiThreading Library
 #include <RobotServos.h> //Servo Library
 #include <MPU9250.h> //Gyro Library
@@ -5,17 +6,20 @@
 #include <RotaryEncoder.h> //Rotary Encoder Library
 #include <SoftwareSerial.h> //Bluetooth Library
 
-#define ledPin 13
-RobotServos servos(6, 5, 5);
-DistanceEstimator dF(8, 7);
-DistanceEstimator dR(10, 9);
+RGBLamp led;
+RobotServos servos(7, 6, 5);
+DistanceEstimator dF(9, 8);
+DistanceEstimator dR(11, 10);
 //MPU9250 mpu;
-SoftwareSerial groundStation(11, 12); //Create a serial connection with TX and RX on these pins
+SoftwareSerial groundStation(12, 13); //Create a serial connection with TX and RX on these pins
 RotaryEncoder rightEncoder(A2, A3);
 RotaryEncoder leftEncoder(A0, A1);
 
-//Global Variables ------------------------------
 int state = 0; //0, Stop; 1, Forward; 2, Reverse; 3, Turn Left; 4, Turn Right; Other, LED on 
+boolean isConnected = false; //needs to be false
+int nullCount = 0;
+int baudRate = 9600;
+
 int totalTravelR = 0;
 int totalTravelL = 10;
 
@@ -24,15 +28,13 @@ int stopValLeft = 0;
 int servoTolerance = 2; //number of encoder step shifts when stopped
 
 double stoppingBenchmark = 2; //cm
-int baudRate = 9600;
-int nullCount = 0;
-boolean isConnected = false;
-//-----------------------------------------------
+byte someColors[6][3] = { {0, 0, 255}, {0, 255, 0}, {255, 0, 255}, {255, 147, 255}, {255, 255, 0}, {224, 5, 97} };
 
 void setup(){
-  pinMode(ledPin, OUTPUT);
   Serial.begin(baudRate); //Initialize communications to the serial monitor in the Arduino IDE
   groundStation.begin(baudRate); //Initialize communications with the bluetooth module
+  led.begin(COMMON_ANODE, 5, 4, 3);
+  led.blink(Color::Red);
   
   //Wire.begin();
   //delay(2000);
@@ -47,25 +49,27 @@ void recieveData(){
       newState = groundStation.read() - '0';
     }
     if(newState > -1 && newState < 5){
-      //Serial.println("Recieved New State");
+      //Serial.println("Recieved New State: " + newState);
       state = newState;
       isConnected = true;
+      led.setColor(someColors[state][0], someColors[state][1], someColors[state][2]);
+  
     }
   }
   else{
     nullCount++;
-  }
-
-  if(nullCount > 1 && isConnected){
-    isConnected = false;
-    state = 0;
-    Serial.println("Disconnected");
+    if(nullCount > 25 && isConnected){
+      isConnected = false;
+      state = 0;
+      led.blink(Color::Red);
+      Serial.println("Disconnected");
+    }
   }
 }
 
 void transmitData(){
   if(isConnected){
-    groundStation.println(String(millis()) + "," + String(state) + "," + String(dR.getAverage()) + "," + String(dR.getAverage()) + "," + String(-totalTravelL) + "," + String(totalTravelR) + "," + String(random(0,270)));
+    groundStation.println(":" + String(millis()) + "," + String(state) + "," + String(dR.getAverage()) + "," + String(dR.getAverage()) + "," + String(-totalTravelL) + "," + String(totalTravelR) + "," + String(random(0,270)) + ":");
     //Example --> 12330,0,12.0,34.0,-48,-39,20.342
     //Serial.println(String(millis()) + "," + String(state) + "," + String(" ") + "," + String(" ") + "," + String(totalTravelL) + "," + String(totalTravelR) + "," + String(120.20));
     Serial.println("Transmitted Data");
@@ -86,9 +90,6 @@ void encoderCheck(){
     //state = 0; //stops
     //Serial.println("Error: Too much encoder movement while in 'Stopped' State --> increase servo tolerance");
   //}
-
-  //check state and encoder direction
-  
 }
 
 void refreshVariables(){
@@ -113,13 +114,12 @@ void refreshVariables(){
   }
 }
 
-
 TimedAction recieve = TimedAction(100, recieveData);
 TimedAction transmit = TimedAction(500, transmitData);
 
 void loop(){
   refreshVariables();
+  led.update(); //Run continuously
   recieve.check();
   transmit.check();
-  Serial.println(state);
 }
